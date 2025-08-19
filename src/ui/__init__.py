01cb485485f7121263f80ed5709626ba93ebbb2f
TrillSpillTrilly <Trillspilltrilly@gmail.com>
2025-08-19 08:36:36 -0500

    Create __init__.py with initial UI components

---
+++ src/ui/__init__.py
@@ -0,0 +1,8 @@
+from .advanced_components import EnhancedNotebookUI
+from .visualization import DashboardComponents
+from .modern_elements import ModernUIElements
+from .collaborative import CollaborativeFeatures
+from .layouts import LayoutManager
+
+__version__ = '0.1.0'
+__all__ = ['EnhancedNotebookUI', 'DashboardComponents', 'ModernUIElements', 'CollaborativeFeatures', 'LayoutManager']
